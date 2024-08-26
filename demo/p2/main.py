7dd4174cffa8aab93ca438dc79e9f7c0e3af7d79
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-08-26 16:24:45 +0300

    Fix problem 2

--- demo/p2/main.py
+++ demo/p2/main.py
@@ -9,9 +9,9 @@ N = (24, 24) # sheet resolution
 T = 400 # number of time steps to simulate
 K = 1 # thermal diffusivity
 
-# boundary condition (constant corner temperature)
-# top left, top right, bottom left, bottom right
-U0 = (0, 50, 50, 100) 
+# boundary condition (constant edge temperature)
+# top, left, right, bottom
+U0 = (0, 50, 50, 100)
 
 # choose such time step that numerical stability is ensured
 DT = (DX ** 2) * (DY ** 2) / (2 * K * (DX**2 + DY**2))
@@ -21,18 +21,18 @@ DT = (DX ** 2) * (DY ** 2) / (2 * K * (DX**2 + DY**2))
 u = np.zeros([*N, T])
 
 # boundary conditions
-u[0, 0, :] = U0[0]
-u[0, -1, :] = U0[1]
-u[-1, 0, :] = U0[2]
-u[-1, -1, :] = U0[3]
+u[:,0,:] = U0[0]
+u[0,:,:] = U0[1]
+u[-1,:,:] = U0[2]
+u[:,-1,:] = U0[3]
 
 for t in range(T - 1):
-  for x in range(N[0]):
-    for y in range(N[1]):
-      
-      d2ux = 0 if x == 0 or x == N[0] - 1 else (u[x + 1, y, t] - 2 * u[x, y, t] + u[x - 1, y, t]) / (DX ** 2) 
-      d2uy = 0 if y == 0 or y == N[1] - 1 else (u[x, y + 1, t] - 2 * u[x, y, t] + u[x, y - 1, t]) / (DY ** 2)
-      
+  
+  # only update cells that are not on the boundary
+  for x in range(1, N[0] - 1):
+    for y in range(1, N[1] - 1):
+      d2ux = (u[x + 1, y, t] - 2 * u[x, y, t] + u[x - 1, y, t]) / (DX ** 2) 
+      d2uy = (u[x, y + 1, t] - 2 * u[x, y, t] + u[x, y - 1, t]) / (DY ** 2)
       u[x, y, t + 1] = u[x, y, t] + K * DT * (d2ux + d2uy)
 
 # render animation
