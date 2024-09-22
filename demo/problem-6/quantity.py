60761a10355cdfe26002416f7ea38f528b59f8bc
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-09-22 20:39:37 +0300

    improve quantity graph

--- demo/problem-6/quantity.py
+++ demo/problem-6/quantity.py
@@ -1,10 +1,9 @@
 # %%
 
 import numpy as np
-import numpy.typing as npt
 import matplotlib.pyplot as plt
 
-c = np.load(f'saves/optimized/[n,t,cs]=[100,10000,0]-optimized.npy')
+c = np.load(f'saves/suboptimal/[n,t,cs]=[10,100,0].npy')
 c1, c2, c3 = c[0], c[1], c[2]
 
 SIZE_X, SIZE_Y, T = c.shape[1:]
@@ -15,12 +14,12 @@ c2_quantity = np.sum(c2, axis=(0, 1))
 c3_quantity = np.sum(c3, axis=(0, 1))
 
 ts = np.arange(0, T, 1)
-plt.title(f'Quantity of elements c1, c2, c3 over time')
+plt.title(f'Quantity of elements $c_1$, $c_2$, $c_3$ over time.\n Configuration: [n, t, cs]=[{SIZE_X}, {T}, 0]')
 plt.xlabel('t')
-plt.ylabel('c1, c2, c3 quantity')
-plt.plot(ts, c1_quantity, label=f'c1')
-plt.plot(ts, c2_quantity, label=f'c2')
-plt.plot(ts, c3_quantity, label=f'c3')
-plt.plot(ts, c1_quantity + c3_quantity + c2_quantity, label=f'c1 + c2 + c3')
-plt.legend()
-# %%
+plt.ylabel('$c_1$, $c_2$, $c_3$ quantity')
+plt.plot(ts, c1_quantity, label=f'$c_1$')
+plt.plot(ts, c2_quantity, label=f'$c_2$')
+plt.plot(ts, c3_quantity, label=f'$c_3$')
+plt.plot(ts, c1_quantity + c3_quantity + c2_quantity, label=f'$\Sigma c_i$')
+plt.legend(loc='upper left')
+ # %%
