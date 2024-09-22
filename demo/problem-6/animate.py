17f305e08723ad854c28b27d8e30e4628038ebc1
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-09-22 19:56:14 +0300

    change scripts to load from compressed file

--- demo/problem-6/animate.py
+++ demo/problem-6/animate.py
@@ -8,20 +8,26 @@ import matplotlib.animation as plta
 
 plt.rcParams['animation.ffmpeg_path'] = 'C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin\\ffmpeg.exe'
 
-L, T = 1, 10000
-D = 1/4 # Diffusivity constant
-DX, DY = 0.01, 0.01
-SIZE_X, SIZE_Y = int(L / DX), int(L / DY)
+LOAD_DIR = 'saves/optimized'
+FILENAME = '[n,t,cs]=[100,10000,0]-optimized'
+c = np.load(f'{LOAD_DIR}/{FILENAME}.npy')
 
-c1 = np.load(f'saves/c1_{SIZE_X}x{SIZE_Y}_t{T}-adjusted-coef.npy')
-c2 = np.load(f'saves/c2_{SIZE_X}x{SIZE_Y}_t{T}-adjusted-coef.npy')
-c3 = np.load(f'saves/c3_{SIZE_X}x{SIZE_Y}_t{T}-adjusted-coef.npy')
+print(c.shape)
+
+SIZE_X, SIZE_Y = c.shape[1], c.shape[2]
+DX, DY, T = 1 / SIZE_X, 1 / SIZE_Y, c.shape[3],
+D = 1/4 # Diffusivity constant
+
+c1, c2, c3 = c[0], c[1], c[2]
+
+print(f'T={T}, SIZE=({SIZE_X}, {SIZE_Y})')
+print(c1.shape, c2.shape, c3.shape)
 
 DT1 = 1 / (2 * D * (1/DX**2 + 1/DY**2) + 3 * np.max(c2[:, :, 0]))
 DT2 = 1 / (2 * D * (1/DX**2 + 1/DY**2) + 5 * np.max(c1[:, :, 0]))
 DT = np.min([DT1, DT2]) 
 
-frame_stride = 50 # only render every 50 frames
+frame_stride = 50 # only render every 50 frames for long simulations
 
 def animate_parametrized(frame: int, c: npt.NDArray[np.float64], c_index: int):
   t = frame * frame_stride
@@ -29,7 +35,7 @@ def animate_parametrized(frame: int, c: npt.NDArray[np.float64], c_index: int):
   plt.xlabel('x')
   plt.ylabel('y')
   plt.title(f'Values of c_{c_index}(x, y, t) at time t={DT * (t + frame_stride):.02f}')
-  plt.imshow(c[:, :, t].T, cmap='inferno', extent=[0, L, 0, L], vmin=0, vmax=1)
+  plt.imshow(c[:, :, t].T, cmap='inferno', extent=[0, 1, 0, 1], vmin=0, vmax=1)
   plt.colorbar()
 
 FFwriter = plta.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
@@ -38,6 +44,6 @@ for i, c in enumerate([c1, c2, c3]):
   animate_func: Callable[[int], None] = lambda frame: animate_parametrized(frame, c, i+1)
   animation = plta.FuncAnimation(plt.gcf(), animate_func, frames=int(T/frame_stride), interval=20)
   FFwriter = plta.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
-  animation.save(f'videos/c{i+1}_{SIZE_X}x{SIZE_Y}_t{T}-adjusted-coef.mp4', writer=FFwriter)
+  animation.save(f'videos/optimized/c{i+1}-{FILENAME}.mp4', writer=FFwriter)
 
 # %%
--- demo/problem-6/quantity.py
+++ demo/problem-6/quantity.py
@@ -4,12 +4,11 @@ import numpy as np
 import numpy.typing as npt
 import matplotlib.pyplot as plt
 
-L = 1
-SIZE_X, SIZE_Y, T = 10, 10, 100
+c = np.load(f'saves/optimized/[n,t,cs]=[100,10000,0]-optimized.npy')
+c1, c2, c3 = c[0], c[1], c[2]
 
-c1 = np.load(f'saves/c1_{SIZE_X}x{SIZE_Y}_t{T}.npy')
-c2 = np.load(f'saves/c2_{SIZE_X}x{SIZE_Y}_t{T}.npy')
-c3 = np.load(f'saves/c3_{SIZE_X}x{SIZE_Y}_t{T}.npy')
+SIZE_X, SIZE_Y, T = c.shape[1:]
+print(SIZE_X, SIZE_Y, T)
 
 c1_quantity = np.sum(c1, axis=(0, 1))
 c2_quantity = np.sum(c2, axis=(0, 1))
@@ -24,3 +23,4 @@ plt.plot(ts, c2_quantity, label=f'c2')
 plt.plot(ts, c3_quantity, label=f'c3')
 plt.plot(ts, c1_quantity + c3_quantity + c2_quantity, label=f'c1 + c2 + c3')
 plt.legend()
+# %%
