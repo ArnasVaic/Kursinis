0435fa01fe4ae536163332c7de93c6048ba5b973
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-10-11 11:04:24 +0300

    format code, add hd video examples

--- demo/problem-6/main.py
+++ demo/problem-6/main.py
@@ -12,8 +12,9 @@ B = solver_config['B']
 t_mix = solver_config['t_mix']
 
 N, M = int(W / dx) + 1, int(H / dy) + 1
+print(N, M)
 c1_init, c2_init = setup_initial_c1c2(N, M)
-c = solve(W ,H, dx, dy, D, c1_init, c2_init, 0.02, t_mix, B, debug=True)
+c = solve(W, H, dx, dy, D, c1_init, c2_init, 0.02, t_mix, B, debug=True)
 
 # skip as many frames as needed
 # to keep file sizes small and 
