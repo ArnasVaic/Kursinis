aeb59d2f1b486acb85678aa3cebb929032e8ac0d
Arnas <arnasvaic7@gmail.com>
2024-10-24 22:36:35 +0300

    progress

--- demo/problem-6/dx-error.py
+++ demo/problem-6/dx-error.py
@@ -6,7 +6,7 @@ from solver import *
 from initial_config import *
 from tqdm import tqdm
 
-W, H, D = 1, 1, 0.0001
+W, H, D = 1, 1, 0.001
 M = 30
 dy = H / (M - 1)
 
@@ -16,17 +16,25 @@ gautos Oilerio metodu su skirtingais žingsniais $\\Delta x$\n
 kiti parametrai bendri $\\Delta y=\\frac{{{H}}}{{{M-1}}},D={D}$""")
 plt.xlabel('$t$')
 plt.ylabel('$q(t)$')
-for N in tqdm([40, 36, 28, 24, 20, 16, 10]):
+
+T = 8000
+dt = 0.0004
+
+# all had different amounts of initial 
+for N in tqdm([100, 50, 40, 30, 24, 20, 10]):
+
   dx = W / (N - 1)
   c1_init, c2_init = setup_initial_c1c2(N, M)
-  c = solve(W, H, dx, dy, D, c1_init, c2_init, 0.02, None)
+
+  c = solve(W, H, dx, dy, D, c1_init, c2_init, 0.02, None, T = T, debug=True, dt=dt)
   check_solution(c)
   T = c.shape[1]
+  
   stride = max(1, int(c.shape[1] / 200))
   c1, c2, c3 = c[:, ::stride, :, :]
-  dt = get_dt(dx, dy, D, c1_init.max(), c2_init.max())
+  #dt = get_dt(dx, dy, D, c1_init.max(), c2_init.max())
   ts = np.linspace(0, T * dt, c3.shape[0])
-  q = c3.sum(axis=(1, 2))
+  q = c3.sum(axis=(1, 2)) #/ (c1_init + c2_init).sum()
   plt.plot(ts, q, label=f'$\\Delta x=\\frac{{{W}}}{{{N-1}}}$')
 plt.legend()
-# %%
+#  %%
--- demo/problem-6/solver.py
+++ demo/problem-6/solver.py
@@ -4,9 +4,13 @@ from mixer import mix
 import sys
 
 def get_dt(dx, dy, D, c1_max, c2_max):
+
+  D_x = D / dx
+  D_y = D / dy
+
   return 1 / np.max([
-    2 * D * (dx**-2 + dy**-2) + 3 * c2_max,
-    2 * D * (dx**-2 + dy**-2) + 5 * c1_max
+    2 * (D_x/dx**2 + D_y/dy**2) + 3 * c2_max,
+    2 * (D_x/dx**2 + D_y/dy**2) + 5 * c1_max
   ])
 
 def check_solution(c):
@@ -37,33 +41,47 @@ def solve(
   W, 
   H, 
   dx, 
-  dy, 
+  dy,
   D, 
   c1_init, 
   c2_init, 
   threshold, 
   t_mix=None, 
-  B=2, 
-  debug=False):
+  B=2,
+  debug=False,
+  T=None,
+  dt=None):
+
+  D_x, D_y = D / dx, D / dy
 
   laplacian_filter = np.array([
-    [        0,                  dy ** -2 ,        0 ],
-    [ dx ** -2, -2 * (dx ** -2 + dy ** -2), dx ** -2 ],
-    [        0,                  dy ** -2 ,        0 ]
+    [        0,                  D_y * dy ** -2 ,        0 ],
+    [ D_x * dx ** -2, -2 * (D_x * dx ** -2 + D_y * dy ** -2), D_x * dx ** -2 ],
+    [        0,                  D_y * dy ** -2 ,        0 ]
   ])
 
   def laplacian(c):
     padded = np.pad(c, (1, 1), 'edge')
     return convolve2d(padded, laplacian_filter, mode='valid')
 
-  dt = get_dt(dx, dy, D, c1_init.max(), c2_init.max())
+  dt_safe = get_dt(dx, dy, D, c1_init.max(), c2_init.max())
+  if debug and dt is not None and dt > dt_safe:
+    print(f'warning: given dt may produce unstable results!')
+    print(f'suggestion: use dt below or equal to {dt_safe:02f}')
 
-  width, height = int(W / dx) + 1, int(H / dy) + 1
+  dt = dt or dt_safe
+
+  width, height = round(W / dx) + 1, round(H / dy) + 1
   c1, c2, c3 = [ c1_init ], [ c2_init ], [ np.zeros((width, height)) ]
 
   if debug:
+    if T is None:
+      print(f'Stopping when quantity of initial elements reaches threshold.')
+    else:
+      print(f'Stopping on time step T={T}.')
     print(f'discrete grid size: ({width}, {height})')
     print(f'dt = {dt}')
+    print(f'D = ({D_x}, {D_y})')
     if t_mix is not None:
       print(f"approximate frame of mixing: {int(t_mix / dt)}")
   
@@ -81,8 +99,8 @@ def solve(
       c1[t], c2[t], c3[t] = mix([c1[t], c2[t], c3[t]], B=B, debug=False)
 
     c1c2 = c1[t][:, :] * c2[t][:, :]
-    c1.append(c1[t][:, :] - 3 * dt * c1c2 + dt * D * laplacian(c1[t][:, :]))
-    c2.append(c2[t][:, :] - 5 * dt * c1c2 + dt * D * laplacian(c2[t][:, :]))
+    c1.append(c1[t][:, :] - 3 * dt * c1c2 + dt * laplacian(c1[t][:, :]))
+    c2.append(c2[t][:, :] - 5 * dt * c1c2 + dt * laplacian(c2[t][:, :]))
     c3.append(c3[t][:, :] + 2 * dt * c1c2)
 
     c1c2_qnt = (c1[t + 1][:, :] + c2[t + 1][:, :]).sum()
@@ -96,7 +114,11 @@ def solve(
 
     # stop reaction when ratio of elements c1, c2 
     # to the initial amount reaches threshold 
-    if c1c2_qnt / c1c2_qnt0 <= threshold:
+    #
+    if T is None and c1c2_qnt / c1c2_qnt0 <= threshold:
+      break
+
+    if T is not None and t == T:
       break
 
     t = t + 1
